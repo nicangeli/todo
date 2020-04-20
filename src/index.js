cc357b772589859a9f5a4e1f7d0620d23d402354
Nick Angeli <nicangeli@gmail.com>
2020-04-20 13:16:04 +0100

    add react setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello wozrld')
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => 'Hello World'
+
+ReactDom.render(<App />, document.querySelector('#app'))
