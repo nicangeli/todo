71bab75937886cd695c837722ef9ca6ae63b8f58
Nick Angeli <nicangeli@gmail.com>
2020-04-20 18:33:27 +0100

    use recordingReducer in store

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 
 import todos from './features/todo/todoSlice'
+import recording from './features/recording/recordingSlice'
 
 const rootReducer = combineReducers({
   todos,
+  recording,
 })
 
 const store = configureStore({
